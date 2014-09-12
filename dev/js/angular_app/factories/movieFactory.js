105f4e5006218389c962390b42e8ee1122f9e6f7
Michael Slater <slatron@gmail.com>
2014-09-11 22:50:10 -0500

    updating API calls

--- dev/js/angular_app/factories/movieFactory.js
+++ dev/js/angular_app/factories/movieFactory.js
@@ -3,9 +3,10 @@ factory('movieFactory', function($http, $q) {
 
   var movieFactoryMethods = {},
       hostUrl = 'https://api.themoviedb.org/3/',
-      method  = 'movie/550',
-      query   = '?api_key=4a3af8cc19e83189d99a0748e2663751',
-      url     = hostUrl + method + query;
+      method  = 'discover/movie',
+      key     = '?api_key=4a3af8cc19e83189d99a0748e2663751',
+      query   = '&with_genres=35&year=1993',
+      url     = hostUrl + method + key + query;
 
   movieFactoryMethods.getMovies = function() {
     var deferred = $q.defer();
